6638703b1a08fb2f60c51cb78ee039429c9fa441
GwynethS <102232553+GwynethS@users.noreply.github.com>
2025-01-27 23:54:51 -0500

    cart endpoints added
    put and delete methods

--- src/managers/cart.manager.js
+++ src/managers/cart.manager.js
@@ -14,9 +14,11 @@ export class CartManager {
     }
   }
 
-  async findById(id) {
+  async findById(cartId) {
     try {
-      const result = await this.model.findById(id).populate("products.product");
+      const result = await this.model
+        .findById(cartId)
+        .populate("products.product");
 
       return result;
     } catch (e) {
@@ -34,7 +36,37 @@ export class CartManager {
     }
   }
 
-  async addProductToCart(cartId, productId) {
+  async updateCart(cartId, products) {
+    try {
+      const updatedCart = await this.model
+        .findByIdAndUpdate(cartId, { products }, { new: true })
+        .populate("products.product");
+
+      if (!updatedCart) {
+        throw new Error("Cart not found");
+      }
+
+      return updatedCart;
+    } catch (e) {
+      return { error: true, message: e.message };
+    }
+  }
+
+  async deleteCartProducts(cartId) {
+    try {
+      const updatedCart = await this.model
+        .findByIdAndUpdate(cartId, { $set: { products: [] } }, { new: true })
+        .populate("products.product");
+
+      if (!updatedCart) throw new Error("Cart not found");
+
+      return updatedCart;
+    } catch (e) {
+      return { error: true, message: e.message };
+    }
+  }
+
+  async addCartProduct(cartId, productId) {
     try {
       const cart = await this.model.findById(cartId);
       const product = await ProductModel.findById(productId);
@@ -58,11 +90,63 @@ export class CartManager {
         existingProduct.quantity += 1;
       }
 
-      const cartUpdated = await this.model
+      const updatedCart = await this.model
         .findByIdAndUpdate(cartId, cart, { new: true })
         .populate("products.product");
 
-      return cartUpdated;
+      return updatedCart;
+    } catch (e) {
+      return { error: true, message: e.message };
+    }
+  }
+
+  async updateCartProduct(cartId, productId, quantity) {
+    try {
+      const cart = await this.model.findById(cartId);
+      const product = await ProductModel.findById(productId);
+
+      if (!cart) throw new Error("Cart not found");
+
+      if (!product) throw new Error("Product not found");
+
+      const existingProduct = cart["products"].find(
+        (p) => p["product"].toString() === productId
+      );
+
+      if (!existingProduct) {
+        const insertProduct = {
+          product: productId,
+          quantity,
+        };
+
+        cart["products"].push(insertProduct);
+      } else {
+        existingProduct.quantity = quantity;
+      }
+
+      const updatedCart = await this.model
+        .findByIdAndUpdate(cartId, cart, { new: true })
+        .populate("products.product");
+
+      return updatedCart;
+    } catch (e) {
+      return { error: true, message: e.message };
+    }
+  }
+
+  async deleteCartProduct(cartId, productId) {
+    try {
+      const updatedCart = await this.model
+        .findByIdAndUpdate(
+          cartId,
+          { $pull: { products: { product: productId } } },
+          { new: true }
+        )
+        .populate("products.product");
+
+      if (!updatedCart) throw new Error("Cart not found");
+
+      return updatedCart;
     } catch (e) {
       return { error: true, message: e.message };
     }
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -29,11 +29,48 @@ router.post("/", async (req, res) => {
   res.status(200).json({ payload: result });
 });
 
-router.post("/:cid/product/:pid", async (req, res) => {
+router.put("/:cid", async (req, res) => {
+  try {
+      const { cid } = req.params;
+      const { products } = req.body;
+
+      if (!Array.isArray(products)) {
+          return res.status(400).json({ status: "error", message: "Products must be an array" });
+      }
+
+      const result = await model.updateCart(cid, products);
+
+      if (result?.error) {
+          return res.status(404).json({ status: "error", message: result.message });
+      }
+
+      res.status(200).json({ status: "success", payload: result });
+  } catch (error) {
+      res.status(400).json({ status: "error", message: error.message });
+  }
+});
+
+router.delete("/:cid", async (req, res) => {
+  try {
+      const { cid } = req.params;
+
+      const result = await model.deleteCartProducts(cid);
+
+      if (result?.error) {
+          return res.status(404).json({ status: "error", message: result.message });
+      }
+
+      res.status(200).json({ status: "success", payload: result });
+  } catch (error) {
+      res.status(400).json({ status: "error", message: error.message });
+  }
+});
+
+router.post("/:cid/products/:pid", async (req, res) => {
   try {
     const { cid, pid } = req.params;
 
-    const result = await model.addProductToCart(cid, pid);
+    const result = await model.addCartProduct(cid, pid);
 
     if (result?.error) {
       return res
@@ -46,5 +83,46 @@ router.post("/:cid/product/:pid", async (req, res) => {
     res.status(400).json({ message: e.message });
   }
 });
+
+router.put("/:cid/products/:pid", async (req, res) => {
+  try {
+    const { cid, pid } = req.params;
+    const {quantity} = req.body;
+
+    if(quantity < 1) return res.status(400).json({ message: "The 'quantity' must be a positive number." });
+
+    const result = await model.updateCartProduct(cid, pid, quantity);
+
+    if (result?.error) {
+      return res
+        .status(400)
+        .json({ message: "Can't update product from cart", error: result.message });
+    }
+
+    res.status(200).json({ payload: result });
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+});
+
+router.delete("/:cid/products/:pid", async (req, res) => {
+  try {
+    const { cid, pid } = req.params;
+
+    const result = await model.deleteCartProduct(cid, pid);
+
+    if (result?.error) {
+      return res
+        .status(400)
+        .json({ message: "Can't delete product from cart", error: result.message });
+    }
+
+    res.status(200).json({ payload: result });
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+});
+
+
 
 export default router;
